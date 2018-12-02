43c9148902814ba37f74eadc8726bec8914f7d4a
Fredrik Hansson <gredrik@gmail.com>
2018-12-02 21:18:29 +0100

    Day 2 part 2

--- day2/day2.clj
+++ day2/day2.clj
@@ -1,8 +1,18 @@
-(def f (slurp "./input.txt"))
-(def lines (clojure.string/split-lines f))
+(def lines (clojure.string/split-lines (slurp "./input.txt")))
 (def f (map (comp set vals frequencies) lines))
 (println 
   (* 
     (reduce + (map #(if (% 2) 1 0 ) f)) 
     (reduce + (map #(if (% 3) 1 0 ) f))))
 
+(defn distance [a b] 
+  (count (filter (partial apply not=) (map vector a b))))
+
+(defn common-letters [a b] 
+  (filter (partial apply =) (map vector a b)))
+
+(def pair
+  (first (take 1 (
+    for [i lines j lines :let [d (distance i j)] :when (= d 1)] [i j]))))
+
+(println (clojure.string/join (map first (common-letters (first pair) (last pair)))))
