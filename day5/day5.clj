714c9fa6e1a9e6c9a98517fe07f0b8ffb510e933
Fredrik Hansson <gredrik@gmail.com>
2018-12-05 20:55:10 +0100

    Part 2 and some refactoring

--- day5/day5.clj
+++ day5/day5.clj
@@ -1,5 +1,10 @@
 (def polymer (slurp "./input.txt"))
-(def codes (map int polymer))
+(def units "ABCDEFGHIJKLMNOPQRSTUVWXY")
+(defn to-codes [polymer] (map int polymer))
 (defn react? [a b] (and (some? a) (= (bit-xor a b) 0x20)))
 (defn r [acc c] (if (react? (last acc) c) (pop acc) (conj acc c)))
-(println (count (reduce r [] codes)))
+(defn unit-count [polymer] (count (reduce r [] (to-codes polymer))))
+(defn remove-unit [polymer unit] (clojure.string/replace polymer (re-pattern (str "(?i)" unit)) ""))
+
+(println (unit-count polymer))
+(println (apply min (map (comp unit-count (partial remove-unit polymer)) units))) 
